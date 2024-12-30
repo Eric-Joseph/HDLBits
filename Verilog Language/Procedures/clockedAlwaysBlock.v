0fd02a49e334d0a2e1d14850316d625074c22c36
Eric Joseph <86331295+Eric-Joseph@users.noreply.github.com>
2024-12-30 04:08:05 -0500

    Create clockedAlwaysBlock.v

---
+++ Verilog Language/Procedures/clockedAlwaysBlock.v
@@ -0,0 +1,11 @@
+module top_module(
+    input clk,
+    input a,
+    input b,
+    output wire out_assign,
+    output reg out_always_comb,
+    output reg out_always_ff   );
+
+endmodule
+
+// UNDER DEVELOPMENT. WILL FINISH AT A LATER DATE
