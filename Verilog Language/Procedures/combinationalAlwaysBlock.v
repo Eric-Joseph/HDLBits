301f811b809c811de7de1cf88e49c9272d7ded39
Eric Joseph <86331295+Eric-Joseph@users.noreply.github.com>
2024-12-30 04:05:37 -0500

    Update combinationalAlwaysBlock.v

--- Verilog Language/Procedures/combinationalAlwaysBlock.v
+++ Verilog Language/Procedures/combinationalAlwaysBlock.v
@@ -1,1 +1,11 @@
+module top_module(
+    input a, 
+    input b,
+    output wire out_assign,
+    output reg out_alwaysblock
+);
+    
+    assign out_assign = a & b;            // Assign Statement
+  always @(*) out_alwaysblock = a & b;  //Combinational Always Block
 
+endmodule
